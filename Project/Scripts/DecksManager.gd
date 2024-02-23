624de90676141b2c9d4afe154fcb8dea6e4f8f74
Vicieta31 <danicastillor04@gmail.com>
2024-02-24 00:14:23 +0100

    ShowCards Input Click Cursor
    funcion showcards que enseña todas las cartas con sus sprites respectivos creada.
    Input Click que detecta el click de raton y la "X" en el mando creada.
    Nodo Cursor creado

--- Project/Scripts/DecksManager.gd
+++ Project/Scripts/DecksManager.gd
@@ -1,6 +1,8 @@
 extends Node
 @export var deckJ1 = []
 @export var deckJ2 = []
+var spritesCards = ["res://Sprites/fotoperfil.jpg", "res://Sprites/fuckboyycumcum.jpg", "res://Sprites/lol osea o my god en plan holy shit.jpg", "res://Sprites/man.png", "res://Sprites/merengue.jpg"]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,9 +10,33 @@ func _ready():
 	deckJ2.shuffle()
 	print(deckJ1)
 	print(deckJ2)
-	pass
+	_ShowCards()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _ShowCards():
+	$"../J1Card1".texture = load(spritesCards[deckJ1[0]])
+	$"../J1Card2".texture = load(spritesCards[deckJ1[1]])
+	$"../J1Card3".texture = load(spritesCards[deckJ1[2]])
+	$"../J1Card4".texture = load(spritesCards[deckJ1[3]])
+	$"../J1Card5".texture = load(spritesCards[deckJ1[4]])
+	$"../J2Card1".texture = load(spritesCards[deckJ1[0]])
+	$"../J2Card2".texture = load(spritesCards[deckJ1[1]])
+	$"../J2Card3".texture = load(spritesCards[deckJ1[2]])
+	$"../J2Card4".texture = load(spritesCards[deckJ1[3]])
+	$"../J2Card5".texture = load(spritesCards[deckJ1[4]])
+	
+
+func _input(event):
+	print(event.as_text())
+	
+func _clickCard():
+	
+	# if clicado:
+	# get carta onj
+	#guardar en array monstruos jugador to fight
+	# eliminar de array deck jugador
+	#desactivar visibilidad carta clicada
