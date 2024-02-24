2979fb0558cc0e25f16e7049f89549b5ab90810d
Vicieta31 <danicastillor04@gmail.com>
2024-02-24 11:58:51 +0100

    Menu terminado
    Falta poner texturas correctas

--- Project/Scripts/main_manu.gd
+++ Project/Scripts/main_manu.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 func _ready():
 	$CanvasLayer/MarginContainer/VBoxContainer2.visible = false
+	$CanvasLayer/MarginContainer/VBoxContainer2/tutorial_image/Sprite2D.visible = false
 	$CanvasLayer/MarginContainer/VBoxContainer/Button_Play._grabFocus()
 	pass
 
@@ -13,6 +14,8 @@ func _on_button_play_pressed():
 func _on_button_tutorial_pressed():
 	$CanvasLayer/MarginContainer/VBoxContainer.visible = false
 	$CanvasLayer/MarginContainer/VBoxContainer2.visible = true
+	$CanvasLayer/MarginContainer/VBoxContainer2/tutorial_image/Sprite2D.texture = load("res://Sprites/fuckboyycumcum.jpg")
+	$CanvasLayer/MarginContainer/VBoxContainer2/tutorial_image/Sprite2D.visible = true
 	$CanvasLayer/MarginContainer/VBoxContainer2/Return._grabFocus()
 
 func _on_button_exit_pressed():
@@ -22,4 +25,5 @@ func _on_button_exit_pressed():
 func _on_return_pressed():
 	$CanvasLayer/MarginContainer/VBoxContainer.visible = true
 	$CanvasLayer/MarginContainer/VBoxContainer2.visible = false
+	$CanvasLayer/MarginContainer/VBoxContainer2/tutorial_image/Sprite2D.visible = false
 	$CanvasLayer/MarginContainer/VBoxContainer/Button_Play._grabFocus()
