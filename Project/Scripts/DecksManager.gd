c418fa7da11d5e8945deb8b8b4a783137a63e256
Vicieta31 <danicastillor04@gmail.com>
2024-02-24 01:00:01 +0100

    HideCards ShowCards
    Funcion para esconder las cartas
    Funcion para mostrar las cartas

--- Project/Scripts/DecksManager.gd
+++ Project/Scripts/DecksManager.gd
@@ -1,23 +1,29 @@
 extends Node
+# deck inicial de cada uno de los jugadores
 @export var deckJ1 = []
 @export var deckJ2 = []
+#sprites de cada una de las cartas
 var spritesCards = ["res://Sprites/fotoperfil.jpg", "res://Sprites/fuckboyycumcum.jpg", "res://Sprites/lol osea o my god en plan holy shit.jpg", "res://Sprites/man.png", "res://Sprites/merengue.jpg"]
+# cartas seleccionadas para combate de los jugadores
+var selectedCardsJ1 = []
+var selectedCardsJ2 = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# desordena las barajas de los jugadores
 	deckJ1.shuffle()
 	deckJ2.shuffle()
 	print(deckJ1)
 	print(deckJ2)
-	_ShowCards()
+	_ChangeCardSprite()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _ShowCards():
+# Cambia la textura a cada una de las cartas de los jugadores
+func _ChangeCardSprite():
 	$"../J1Card1".texture = load(spritesCards[deckJ1[0]])
 	$"../J1Card2".texture = load(spritesCards[deckJ1[1]])
 	$"../J1Card3".texture = load(spritesCards[deckJ1[2]])
@@ -28,7 +34,30 @@ func _ShowCards():
 	$"../J2Card3".texture = load(spritesCards[deckJ1[2]])
 	$"../J2Card4".texture = load(spritesCards[deckJ1[3]])
 	$"../J2Card5".texture = load(spritesCards[deckJ1[4]])
+
+func _HideCards():
+	$"../J1Card1".visible = false
+	$"../J1Card2".visible = false
+	$"../J1Card3".visible = false
+	$"../J1Card4".visible = false
+	$"../J1Card5".visible = false
+	$"../J2Card1".visible = false
+	$"../J2Card2".visible = false
+	$"../J2Card3".visible = false
+	$"../J2Card4".visible = false
+	$"../J2Card5".visible = false
 	
+func _ShowCards():
+	$"../J1Card1".visible = true
+	$"../J1Card2".visible = true
+	$"../J1Card3".visible = true
+	$"../J1Card4".visible = true
+	$"../J1Card5".visible = true
+	$"../J2Card1".visible = true
+	$"../J2Card2".visible = true
+	$"../J2Card3".visible = true
+	$"../J2Card4".visible = true
+	$"../J2Card5".visible = true
 
 func _input(event):
 	print(event.as_text())
@@ -40,3 +69,6 @@ func _clickCard():
 	#guardar en array monstruos jugador to fight
 	# eliminar de array deck jugador
 	#desactivar visibilidad carta clicada
+	pass
+
+
