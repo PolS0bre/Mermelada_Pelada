9fd3200276c8adc6dc42ab739f4ca4229d2b61ad
Vicieta31 <danicastillor04@gmail.com>
2024-02-24 10:42:05 +0100

    MainMenu Logic
    Los botones son funcionales
    Falta crear todo lo que es arte, la parte del tutorial, cambiar los sprites a los botones, etc

---
+++ Project/Scripts/main_manu.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+func _ready():
+	$CanvasLayer/MarginContainer/VBoxContainer/Button_Play._grabFocus()
+	pass
+
+func _on_button_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/cards_scene.tscn")
+
+
+func _on_button_tutorial_pressed():
+	$CanvasLayer/MarginContainer/VBoxContainer.visible = false
+	$CanvasLayer/MarginContainer/VBoxContainer2.visible = true
+	$CanvasLayer/MarginContainer/VBoxContainer2/Return._grabFocus()
+
+func _on_button_exit_pressed():
+	get_tree().quit()
+
+
+func _on_return_pressed():
+	$CanvasLayer/MarginContainer/VBoxContainer.visible = true
+	$CanvasLayer/MarginContainer/VBoxContainer2.visible = false
+	$CanvasLayer/MarginContainer/VBoxContainer/Button_Play._grabFocus()
